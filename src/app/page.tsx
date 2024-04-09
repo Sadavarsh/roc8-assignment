14e771ec449fdde16f71e3fba88dea4a1e71e1b0
Sadavarsh Saini <92135085+Sadavarsh@users.noreply.github.com>
2024-04-09 19:55:43 +0530

    update database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,15 +49,15 @@ export default async function Home() {
 }
 
 async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
+  // const latestPost = await api.post.getLatest();
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
+      {/* {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
-      )}
+      )} */}
 
       <CreatePost />
     </div>
